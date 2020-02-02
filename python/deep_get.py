b2a82283b544898b7a3a52a844b3123e9b9fe02c
Marc Mazer <mmazer@freshbooks.com>
2020-02-02 08:30:26 -0500

    Add import for python reduce snippet

--- python/deep_get.py
+++ python/deep_get.py
@@ -3,6 +3,8 @@
     deep_get(dict, 'key1', 'key2', 'key3')
 """
 
+from functool import reduce
+
 
 def deep_get(dictionary, *keys):
     return reduce(lambda d, key: d.get(key) if d else None, keys, dictionary)
